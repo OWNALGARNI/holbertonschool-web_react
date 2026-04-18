e8da5f47dff6d36b4a9a1d2a11e9d3ad763550bd
OWNALGARNI <s.xooxs@hotmail.com>
2026-04-18 14:58:14 +0300

    update task_1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [propName: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -14,3 +18,14 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
